7858af8c66d71394aa387397865020b73b505b87
Poreddy pavan kumar <pavanporeddy85@gmail.com>
2024-09-18 09:24:13 +0530

    Update WordCounter.java

--- WordCounter.java
+++ WordCounter.java
@@ -3,13 +3,9 @@ import java.util.Scanner;
 public class WordCounter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-
-        String[] words = input.split("\\s+"); 
-        int wordCount = words.length;
-
-        System.out.println("Number of words:" + wordCount);
-    } 
+        String str = scanner.nextLine();
+        String[] words = str.split("\\s+");
+        System.out.println("Number of words: " + words.length);
+    }
 }
